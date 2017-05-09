ee1d74e9d2bfa77db29c123b9fbae08e3e762d33
Shaheer Mir <shaheermir0@gmail.com>
2017-05-08 22:37:10 -0400

    Working on queries

--- UpStarMusic/database/queries/SearchArtists.js
+++ UpStarMusic/database/queries/SearchArtists.js
@@ -9,4 +9,18 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+  const query = Artist.find({})
+    .sort({ [sortProperty]: 1 })
+    .skip(offset)
+    .limit(limit);
+
+  return Promise.all([query, Artist.count()])
+    .then(result => {
+      return {
+        all: result[0],
+        count: result[1],
+        offset,
+        limit
+      };
+    });
 };
