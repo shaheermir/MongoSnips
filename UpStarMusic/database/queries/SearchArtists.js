44f4658b89c6ab41ed42841bb031e8186e97604c
Shaheer Mir <shaheermir0@gmail.com>
2017-05-09 23:31:37 -0400

    Finished up remaining queries.

--- UpStarMusic/database/queries/SearchArtists.js
+++ UpStarMusic/database/queries/SearchArtists.js
@@ -9,12 +9,12 @@ const Artist = require('../models/artist');
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
-  const query = Artist.find({})
+  const query = Artist.find(buildQuery(criteria))
     .sort({ [sortProperty]: 1 })
     .skip(offset)
     .limit(limit);
 
-  return Promise.all([query, Artist.count()])
+  return Promise.all([query, Artist.find(buildQuery(criteria)).count()])
     .then(result => {
       return {
         all: result[0],
@@ -23,4 +23,28 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
         limit
       };
     });
+};
+
+const buildQuery = (criteria) => {
+  const query = {};
+
+  if (criteria.name) {
+    query.$text = { $search: criteria.name };
+  }
+
+  if (criteria.age) {
+    query.age = {
+      $gte: criteria.age.min,
+      $lte: criteria.age.max
+    };
+
+    if (criteria.yearsActive) {
+      query.yearsActive = {
+        $gte: criteria.yearsActive.min,
+        $lte: criteria.yearsActive.max
+      };
+    }
+  }
+
+  return query;
 };
