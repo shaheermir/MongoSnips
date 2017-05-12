4ffa4368ab599ec2ff2c7368fe115727fe04d06e
Shaheer Mir <shaheermir0@gmail.com>
2017-05-11 21:11:18 -0400

    Finished CRUD queries

--- Liift/app.js
+++ Liift/app.js
@@ -4,11 +4,18 @@ const routes = require('./routes/routes')
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost/liift')
+
+if (process.env.NODE_ENV !== 'test') {
+  mongoose.connect('mongodb://localhost/liift')
+}
 
 const app = express()
 
 app.use(bodyParser.json())
 routes(app)
+
+app.use((err, req, res, next) => {
+  res.status(422).send({ error: err.message })
+})
 
 module.exports = app
